3e08f14024c373eb4c8a6b4c0139a13669fe7385
Gyanendra Chaudhary <--replace-all>
2025-01-25 21:34:41 +0545

    ai chat bot

--- Tools.AppHost/Program.cs
+++ Tools.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.TextExtractor>("textextractor");
 
+builder.AddProject<Projects.AiChatBot>("aichatbot");
+
 builder.Build().Run();
